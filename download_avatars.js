d9f8aac1f7545e1ffcd791722cd6b1b992dd60f1
Brian Holden <bkavuh@gmail.com>
2018-03-19 23:05:11 +0000

    stage 2 done

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,6 @@
 var request = require('request');
+var fs = require('fs');
+var counter = 1;
 var autho = require('./secret.js');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,11 +14,35 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+    var exit = '';
+    data.forEach(function(urls){
+      exit += urls.avatar_url + '\n';
+    });
+    cb(err, exit);
+    console.log("Done pulling avatars");
   });
+}
+
+function downloadImageByURL(url, filePath) {
+
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note
+       })
+       .pipe(fs.createWriteStream(filePath+counter+'.jpg'));
+       counter++;
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  var tokens = result.split('\n');
+  tokens.pop();
+  tokens.forEach(function(entry) {
+    var path = './avatar';
+    downloadImageByURL(entry, path);
+  });
 });
